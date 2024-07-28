a614988f9e9debd2088b6f260645c78c8bf887a7
محمدرضا فیروزی <109149767+MohammadrezaFirouzi@users.noreply.github.com>
2024-07-28 15:00:52 +0330

    Update requests.php

--- network/requests.php
+++ network/requests.php
@@ -1,13 +1,38 @@
-<?php 
+<?php
 
 
 
-class Requests{
+class Requests
+{
 
 
     public static function getApiUrl()
     {
-        $urls = json_decode(file_get_contents("https://getdcmess.iranlms.ir/"), true)['data']['API'];
+        $urls = [
+            "https://messengerg2c1.iranlms.ir",
+            "https://messengerg2c2.iranlms.ir",
+            "https://messengerg2c3.iranlms.ir",
+            "https://messengerg2c4.iranlms.ir",
+            "https://messengerg2c5.iranlms.ir",
+            "https://messengerg2c6.iranlms.ir",
+            "https://messengerg2c7.iranlms.ir",
+            "https://messengerg2c8.iranlms.ir",
+            "https://messengerg2c9.iranlms.ir",
+            "https://messengerg2c10.iranlms.ir",
+            "https://messengerg2c11.iranlms.ir",
+            "https://messengerg2c12.iranlms.ir",
+            "https://messengerg2c13.iranlms.ir",
+            "https://messengerg2c14.iranlms.ir",
+            "https://messengerg2c15.iranlms.ir",
+            "https://messengerg2c16.iranlms.ir",
+            "https://messengerg2c17.iranlms.ir",
+            "https://messengerg2c18.iranlms.ir",
+            "https://messengerg2c19.iranlms.ir",
+            "https://messengerg2c20.iranlms.ir",
+            "https://messengerg2c21.iranlms.ir",
+            "https://messengerg2c22.iranlms.ir",
+            "https://messengerg2c23.iranlms.ir"
+        ];
         return $urls[array_rand($urls)];
     }
 
@@ -19,42 +44,30 @@ class Requests{
     }
 
 
-    public static function sendRequest($data) {
+    public static function sendRequest($data)
+    {
+        $ch = curl_init(self::getApiUrl());
 
-        $head = array(
-            "Accept-Encoding: gzip",
-            "Connection: Keep-Alive",
-            "User-Agent: okhttp/3.12.1",
-            "Referer: https://web.rubika.ir/",
-            "Content-Type: application/json; charset=utf-8"
-        );
-    
-        while (true) { 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, self::getApiUrl());
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-            
-            $result = json_decode($response, true);
-    
-            if ($status == 200) {
-                return json_decode($response, true);
-            } else {
-                continue;
-            }
-        }
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
+            CURLOPT_HTTPHEADER => [
+                "Accept-Encoding: gzip",
+                "Connection: Keep-Alive",
+                "User-Agent: okhttp/3.12.1",
+                "Referer: https://web.rubika.ir/",
+                "Content-Type: application/json; charset=utf-8"
+            ],
+            CURLOPT_POSTFIELDS => json_encode($data)
+        ]);
 
+        $response = curl_exec($ch);
+        
+        $error = curl_error($ch);
+        curl_close($ch);
 
+        return $error ? 'Error:' . $error : json_decode($response, true);
     }
-
-
-
-
 }
-
-?>
